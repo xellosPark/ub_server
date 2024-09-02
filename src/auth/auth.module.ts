ecb65f451d7d973be1bae16cf6723411a2909d82
xellosPark <yukihiro81@naver.com>
2024-09-02 22:20:26 +0900

    개발환경 기능 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,15 +8,18 @@ import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt'; // JwtStrategy 추가
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Auth]), // TypeORM을 사용해 Auth 엔티티를 연결
     PassportModule.register({ defaultStrategy: 'jwt' }), // JWT 기본 전략 설정 // Passport 모듈 추가
     JwtModule.register({
-      secret: 'Secret1234', // JWT 서명에 사용될 비밀 키 (환경 변수로 설정 권장)
+      secret: process.env.JWT_SECRET || jwtConfig.secret, // JWT 서명에 사용될 비밀 키 (환경 변수로 설정 권장)
       signOptions: {
-        expiresIn: '1h', // Access Token 유효 기간 설정 (짧은 시간)2m 2분 1h 1시간
+        expiresIn: jwtConfig.expiresIn, // Access Token 유효 기간 설정 (짧은 시간)2m 2분 1h 1시간
       },
     }),
   ],
--- src/configs/TypeORM.config.ts
+++ src/configs/TypeORM.config.ts
@@ -1,15 +1,29 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
 
+const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',            // 데이터베이스 유형
-  host: 'localhost',        // 데이터베이스 호스트
-  port: 3306,               // 데이터베이스 포트
-  username: 'root',         // 데이터베이스 사용자명
-  password: 'ubisam8877',   // 데이터베이스 비밀번호
-  database: 'admin_board',  // 데이터베이스 이름
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],  // 엔티티 경로
-  synchronize: true,        // 애플리케이션 시작 시 스키마 동기화 여부
+  // type: 'mysql',            // 데이터베이스 유형
+  // host: 'localhost',        // 데이터베이스 호스트
+  // port: 3306,               // 데이터베이스 포트
+  // username: 'root',         // 데이터베이스 사용자명
+  // password: 'ubisam8877',   // 데이터베이스 비밀번호
+  // database: 'admin_board',  // 데이터베이스 이름
+  // entities: [__dirname + '/../**/*.entity.{js,ts}'],  // 엔티티 경로
+  // synchronize: true,        // 애플리케이션 시작 시 스키마 동기화 여부
+  // autoLoadEntities: true,   // 엔티티를 자동으로 로드할지 여부
+  // logging: true,            // 데이터베이스 작업을 로깅할지 여부
+  //////////////////////////////////////////////////////////////////////
+
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: dbConfig.synchronize,
   autoLoadEntities: true,
   logging: true,
 };
